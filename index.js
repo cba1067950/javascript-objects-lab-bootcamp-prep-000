10bbfc4d36de3ae8d0ecce0f92b965bee4afbc9c
Joe Jamsky <cba1067950@yahoo.com>
2019-04-03 01:36:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object.key = value;
-  return object;
+  //var newObj = Object.assign({}, object, {[key]: value});
+  //delete object.key;
+  return object.key = value;
 }
